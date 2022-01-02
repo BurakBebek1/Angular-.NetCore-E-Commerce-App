c0c82149b6d468658b2da016faa38197f9c0a92d
Burak Bebek <burakbebek1994@gmail.com>
2022-01-02 17:46:04 +0300

    API paging, Filtering, Sorting & Searching

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -12,6 +12,7 @@ namespace Core.Interfaces
         Task<T> getByIdAsync(int id);
         Task<IReadOnlyList<T>> ListAllAsync();
         Task<T> GetEntityWithSpec(ISpecification<T> spec);
-        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);   
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+        Task<int> CountAsync(ISpecification<T> spec);  
     }
 }
